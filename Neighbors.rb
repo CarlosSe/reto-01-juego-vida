e3245d5b88032b0de89d3ceef306dd78d056df82
Carlos Segura <carlos.seggch@gmail.com>
2020-06-25 20:27:47 -0500

    Se cambian condicionales se elimina variable
    Se corrige error en condicionales para recorrer todos los elementos, ademas se borra variable no utilizada

--- Neighbors.rb
+++ Neighbors.rb
@@ -1,12 +1,11 @@
 class Neighbors
-  @range_position = (-1..1).to_a.uniq
   
   def self.search(game, position_x, position_y)
     contador = 0
     limit = game.length
     for i in (position_y - 1)..(position_y + 1)
       for j in (position_x - 1)..(position_x + 1)
-        next if i == 0 && j == 0
+        next if i == position_y && j == position_x
         valid_x = valid_position(i, limit)
         valid_y = valid_position(j, limit)
         (valid_x && valid_y) ? contador += game[i][j].value : next
@@ -16,6 +15,6 @@ class Neighbors
   end
 
   def self.valid_position(position, limit)
-    true if position >= 0 && position < (limit - 1)
+    true if position >= 0 && position < limit
   end
 end
